8248c2b742b5e62e93abc4c773ad6adf566e9614
haunguyen79 <hauhonguyen2001@gmail.com>
2026-03-05 17:12:58 +0700

    Add friend APIs (routes + controllers)

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,7 +4,7 @@ import Session from "../models/Session.js";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
 
-const ACCESS_TOKEN_TTL = "30s";
+const ACCESS_TOKEN_TTL = "30m";
 const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000; // 14 ngày (Tính theo mili giây)
 
 export const signUp = async (req, res) => {
@@ -115,7 +115,7 @@ export const signOut = async (req, res) => {
     // Lấy refreshToken từ cookie
     const token = req.cookies?.refreshToken;
 
-    if (!token) {
+    if (token) {
       //  Xóa refresh token trong Session
       await Session.deleteOne({ refreshToken: token });
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./libs/db.js";
 import authRoute from "./routers/authRoute.js";
 import userRoute from "./routers/userRouter.js";
+import friendRoute from "./routers/friendRoute.js";
 import cookieParser from "cookie-parser";
 import { protectedRoute } from "./middlewares/authMiddleware.js";
 import cors from "cors";
@@ -23,6 +24,7 @@ app.use("/api/auth", authRoute);
 //Private Routes
 app.use(protectedRoute); // Middleware bảo vệ các route sau
 app.use("/api/users", userRoute);
+app.use("/api/friends", friendRoute)
 
 connectDB().then(() => {
   app.listen(PORT, () => {
