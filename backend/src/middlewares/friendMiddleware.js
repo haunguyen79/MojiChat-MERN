54c4891e246c339b1d69f06dc1ba8f20168bf2dd
haunguyen79 <hauhonguyen2001@gmail.com>
2026-03-10 10:45:42 +0700

    Add Messages APIs (sendGroupMessage)

--- backend/src/middlewares/friendMiddleware.js
+++ backend/src/middlewares/friendMiddleware.js
@@ -12,7 +12,9 @@ export const checkFriendship = async (req, res, next) => {
     const memberIds = req.body?.memberIds ?? []; // Chỉ là bạn bè mới được thêm vào nhóm và ko tạo group với người lạ
 
     if (!recipientId && memberIds.length === 0) {
-      return res.status(400).json({ message: "Cần cung cấp recipientId hoặc memberIds" });
+      return res
+        .status(400)
+        .json({ message: "Cần cung cấp recipientId hoặc memberIds" });
     }
 
     if (recipientId) {
@@ -35,20 +37,51 @@ export const checkFriendship = async (req, res, next) => {
       const [userA, userB] = pair(me, memberId);
       const friend = await Friend.findOne({ userA, userB });
       return friend ? null : memberId;
-    })
+    });
 
-    const results = await Promise.all(friendChecks);  // Promise.all() sẽ đợi tất cả các truy vấn được giải quyết
+    const results = await Promise.all(friendChecks); // Promise.all() sẽ đợi tất cả các truy vấn được giải quyết
 
     const notFriends = results.filter(Boolean); // Lọc ra các thành viên không phải là bạn bè
 
     if (notFriends.length > 0) {
-      return res.status(403).json({ message: "Bạn chỉ có thể thêm bạn bè vào nhóm", notFriends })
+      return res
+        .status(403)
+        .json({ message: "Bạn chỉ có thể thêm bạn bè vào nhóm", notFriends });
     }
 
     return next();
-
   } catch (error) {
     console.error("Lỗi xảy ra khi checkFriendship:", error);
+    return res.status(500).json({ message: "Lỗi hệ thống" });
+  }
+};
+
+export const checkGroupMembership = async (req, res, next) => {
+  try {
+    const { conversationId } = req.body;
+    const userId = req.user._id;
+
+    const conversation = await Conversation.findById(conversationId);
+
+    if (!conversationId) {
+      return res
+        .status(404)
+        .json({ message: "Không tìm thấy cuộc trò chuyện" });
+    }
+
+    const isMember = conversation.participants.some(
+      (p) => p.userId.toString() === userId.toString(),
+    );
+
+    if (!isMember) {
+      return res.status(403).json({ message: "Bạn không ở trong group này." });
+    }
+
+    req.conversation = conversation; // Lưu thông tin cuộc trò chuyện vào req để controller có thể sử dụng
+
+    next();
+  } catch (error) {
+    console.error("Lỗi xảy ra khi checkConversationMembership:", error);
     return res.status(500).json({ message: "Lỗi hệ thống" });
   }
 };
