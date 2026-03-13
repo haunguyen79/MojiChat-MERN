35fe365cf10b9b451ee0b470c6f93d353e7ac468
haunguyen79 <hauhonguyen2001@gmail.com>
2026-03-14 02:37:23 +0700

    feat: Fetch messages from API

--- frontend/src/services/chatService.ts
+++ frontend/src/services/chatService.ts
@@ -1,9 +1,24 @@
 import api from "@/lib/axios";
 import type { ConversationResponse, Message } from "@/types/chat";
 
+interface FetchMessageProps {
+  messages: Message[];
+  cursor?: string; // Lưu con trỏ để phân trang
+}
+
+const pageLimit = 50;
+
 export const chatService = {
   async fetchConversations(): Promise<ConversationResponse> {
     const res = await api.get("/conversations");
     return res.data;
+  },
+
+  async fetchMessages(id: string, cursor?: string): Promise<FetchMessageProps> {
+    const res = await api.get(
+      `/conversations/${id}/messages?limit=${pageLimit}&cursor=${cursor}`,
+    );
+
+    return { messages: res.data.message, cursor: res.data.nextCursor };
   },
 };
